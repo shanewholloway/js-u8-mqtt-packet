ac6036083904370f82de2e82e0f3158b44156931
Shane Holloway <shane.holloway@ieee.org>
2020-04-24 15:54:34 -0600

    MQTT framing and basic layout

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import rpi_jsy from 'rollup-plugin-jsy'
+import rpi_dgnotify from 'rollup-plugin-dgnotify'
+import { terser as rpi_terser } from 'rollup-plugin-terser'
+
+
+const _cfg_ = {
+  plugins: [
+    rpi_dgnotify(),
+    rpi_jsy({defines:{}}),
+  ],
+  external: [],
+}
+
+const cfg_web_min = null && { ... _cfg_,
+  plugins: [ ... _cfg_.plugins, rpi_terser() ]}
+
+const _out_ = { sourcemap: true }
+
+
+const configs = []
+export default configs
+
+
+add_jsy('all')
+add_jsy('index')
+add_jsy('codec', {min: true})
+add_jsy('encode_mqtt', {min: true})
+add_jsy('decode_mqtt', {min: true})
+add_jsy('_test')
+
+
+
+function add_jsy(src_name, opt={}) {
+  configs.push({ ..._cfg_,
+    input: `code/${src_name}.jsy`,
+    output: [
+      { ..._out_, file: `esm/${src_name}.mjs`, format: 'es' },
+      { ..._out_, file: `cjs/${src_name}.cjs`, format: 'cjs', exports: opt.exports || 'named' },
+      opt.name && 
+        { ..._out_, file: `umd/${src_name}.js`, format: 'umd', name: opt.name, exports:'named' },
+    ].filter(Boolean)})
+
+  if (opt.min && cfg_web_min)
+    configs.push({ ...cfg_web_min,
+      input: `code/${src_name}.jsy`,
+      output: [
+        { ..._out_, file: `esm/${src_name}.min.mjs`, format: 'es' },
+        opt.name && 
+          { ..._out_, file: `umd/${src_name}.min.js`, format: 'umd', name: opt.name, exports:'named' },
+      ].filter(Boolean)})
+}
