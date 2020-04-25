213b5d309aa77387fd62c70f86db01555ee727eb
Shane Holloway <shane.holloway@ieee.org>
2020-04-25 13:02:18 -0600

    Simplified exports

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,19 @@ import rpi_dgnotify from 'rollup-plugin-dgnotify'
 import { terser as rpi_terser } from 'rollup-plugin-terser'
 import {builtinModules} from 'module'
 
+const DEBUG = ! true
 
 const _cfg_ = {
   plugins: [
     rpi_dgnotify(),
-    rpi_jsy({defines:{DEBUG: true}}),
+    rpi_jsy({defines:{DEBUG}}),
   ],
   external: id =>
     id.startsWith('u8-mqtt-packet')
       || builtinModules.includes(id),
 }
 
-const cfg_web_min = null && { ... _cfg_,
+const cfg_web_min = DEBUG ? null : { ... _cfg_,
   plugins: [ ... _cfg_.plugins, rpi_terser() ]}
 
 const _out_ = { sourcemap: true }
@@ -24,11 +25,8 @@ const configs = []
 export default configs
 
 
-add_jsy('all')
 add_jsy('index')
 add_jsy('codec', {min: true})
-add_jsy('encode_mqtt', {min: true})
-add_jsy('decode_mqtt', {min: true})
 
 add_jsy('_test')
 add_jsy('_mitm')
