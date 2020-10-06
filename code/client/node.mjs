3248b2fcec2f5a23a757d58e05521b37a98d9043
Shane Holloway <shane.holloway@ieee.org>
2020-10-06 12:15:49 -0600

    Use mqtt_session_ctx for basic clients instead of specific mqtt_session_vN functions

--- code/client/node.mjs
+++ code/client/node.mjs
@@ -1,12 +1,12 @@
-import {mqtt_session_v4, mqtt_session_v5} from '../session.mjs'
+import {mqtt_session_ctx} from '../session.mjs'
 import {MQTTBonesNodeClient as Bones} from './core_node.mjs'
 
 export class MQTTBonesNode_v4 extends Bones {
-  mqtt_session() { return mqtt_session_v4() }
+  mqtt_session() { return mqtt_session_ctx(4) }
 }
 
 export class MQTTBonesNode_v5 extends Bones {
-  mqtt_session() { return mqtt_session_v5() }
+  mqtt_session() { return mqtt_session_ctx(5) }
 }
 
 export default MQTTBonesNode_v4
--- code/client/web.mjs
+++ code/client/web.mjs
@@ -1,12 +1,12 @@
-import {mqtt_session_v4, mqtt_session_v5} from '../session.mjs'
+import {mqtt_session_ctx} from '../session.mjs'
 import {MQTTBonesWebClient as Bones} from './core_web.mjs'
 
 export class MQTTBonesWeb_v4 extends Bones {
-  mqtt_session() { return mqtt_session_v4() }
+  mqtt_session() { return mqtt_session_ctx(4) }
 }
 
 export class MQTTBonesWeb_v5 extends Bones {
-  mqtt_session() { return mqtt_session_v5() }
+  mqtt_session() { return mqtt_session_ctx(5) }
 }
 
 export default MQTTBonesWeb_v4
