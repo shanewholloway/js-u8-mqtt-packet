3ca0b950c4dedce745d89311acc3d8dacf4c7c7c
Shane Holloway <shane.holloway@ieee.org>
2022-12-17 10:25:41 -0700

    Updated dependencies and changed to using @rollup/plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import rpi_dgnotify from 'rollup-plugin-dgnotify'
 import rpi_resolve from '@rollup/plugin-node-resolve'
-import { terser as rpi_terser } from 'rollup-plugin-terser'
+import rpi_terser from '@rollup/plugin-terser'
 import {builtinModules} from 'module'
 
 const _cfg_ = {
@@ -11,8 +11,6 @@ const _cfg_ = {
 let is_watch = process.argv.includes('--watch')
 const cfg_web_min = is_watch ? null : { ... _cfg_,
   plugins: [ ... _cfg_.plugins, rpi_terser() ]}
-
-const _out_ = { sourcemap: true }
 
 
 const configs = []
@@ -35,17 +33,17 @@ function add_module(src_name, opt={}) {
   const input = `code/${src_name}.mjs`
   configs.push({ ..._cfg_, input,
     output: [
-      { ..._out_, file: `esm/${src_name}.mjs`, format: 'es' },
-      { ..._out_, file: `cjs/${src_name}.cjs`, format: 'cjs', exports: 'named' },
+      { file: `esm/${src_name}.mjs`, format: 'es', sourcemap: true },
+      { file: `cjs/${src_name}.cjs`, format: 'cjs', exports: 'named', sourcemap: true },
       opt.name && 
-        { ..._out_, file: `umd/${src_name}.js`, format: 'umd', name: opt.name, exports: 'named' },
+        { file: `umd/${src_name}.js`, format: 'umd', name: opt.name, exports: 'named', sourcemap: true },
     ].filter(Boolean)})
 
   if (opt.min && cfg_web_min)
     configs.push({ ...cfg_web_min, input,
       output: [
-        { ..._out_, file: `esm/${src_name}.min.mjs`, format: 'es' },
+        { file: `esm/${src_name}.min.mjs`, format: 'es', sourcemap: false },
         opt.name && 
-          { ..._out_, file: `umd/${src_name}.min.js`, format: 'umd', name: opt.name, exports: 'named' },
+          { file: `umd/${src_name}.min.js`, format: 'umd', name: opt.name, exports: 'named', sourcemap: false },
       ].filter(Boolean)})
 }
